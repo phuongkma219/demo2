b8a43454c1f27c8429a1479feae05f37518c3bf2
phuongkma219 <54812322+phuongkma219@users.noreply.github.com>
2019-09-04 00:57:30 +0700

    Update Test2.java

--- demo/Test2.java
+++ demo/Test2.java
@@ -1,17 +1,3 @@
-public class Baitaplan1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here 
-        try {
-            int[] a = {1,2,3,4};
-        System.out.println(a[5]);
-        } catch (Exception e) {
-            System.out.println("ko có");
-        }
-       
-    }
+public class Java2 {
 
 }
