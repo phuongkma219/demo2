b268123946544763ddec478eabca5dff34d3d46a
phuongkma <fuong9a@gmail.com>
2019-09-03 23:32:56 +0700

    first commit

---
+++ demo/SNT.java
@@ -0,0 +1,19 @@
+package demo;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+class SNT {
+    int soCanKT;
+    boolean ketQua;
+
+    void kiemTra(){
+        ketQua = true;
+        for (int i = 2; i <soCanKT/2 ; i++) {
+            if (soCanKT % i ==0){
+                ketQua = false;
+            }
+        }
+        System.out.println(ketQua);
+    }
+    sou
+}
